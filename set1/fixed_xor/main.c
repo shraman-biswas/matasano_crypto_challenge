a083a33792b0d26a8b3a519f42647c5f1d5ec41e
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-07 23:33:16 +0530

    completed set 1 question 2

--- set1/fixed_xor/main.c
+++ set1/fixed_xor/main.c
@@ -5,31 +5,43 @@ static void disp_hex(const char *const hex, const int len);
 
 int main(void)
 {
-	printf("[ fixed xor ]\n");
+	printf("[ fixed xor ]\n\n");
 
-	char hex1[strlen(str1) / 2], hex2[strlen(str2) / 2];
-	int len1=0, len2=0;
+	const int len = strlen(str1) / 2;
+	char hex1[len], hex2[len], res[len], str[2 * len];
 
 	/* check if input strings are of equal lengths */
-	len1 = strlen(str1);
-	len2 = strlen(str2);
-	if (len1 != len2) {
+	if ((strlen(str2) / 2) != len) {
 		fprintf(stderr, "unequal input string lengths!\n");
 		exit(EXIT_FAILURE);
 	}
 
 	/* display strings */
-	printf("string 1: %s\nstring 2: %s\n", str1, str2);
+	printf("hex string 1:\n%s\nhex string 2:\n%s\n\n", str1, str2);
 
 	/* convert hex encoded strings to array of hex bytes */
 	str_to_hex(str1, hex1);
 	str_to_hex(str2, hex2);
 
 	/* display array of hex bytes */
-	printf("hex 1: ");
-	disp_hex(hex1, len1 / 2);
-	printf("hex 2: ");
-	disp_hex(hex2, len2 / 2);
+	printf("hex 1:\n");
+	disp_hex(hex1, len);
+	printf("hex 2:\n");
+	disp_hex(hex2, len);
+	printf("\n");
+
+	/* applying fixed xor */
+	fixed_xor(hex1, hex2, res, len);
+
+	/* display result */
+	printf("result:\n");
+	disp_hex(res, len);
+	printf("\n");
+
+	/* display result */
+	printf("result:\n%s\n\n", res);
+	hex_to_str(res, str);
+	printf("hex string:\n%s\n\n", str);
 
 	return EXIT_FAILURE;
 }
