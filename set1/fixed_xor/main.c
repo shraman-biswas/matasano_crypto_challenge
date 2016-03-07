111672d5d635d5cc41cc59441a0ba950f21b31db
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-07 21:22:40 +0530

    display hex encoded string to array of hex bytes result

--- set1/fixed_xor/main.c
+++ set1/fixed_xor/main.c
@@ -1,10 +1,43 @@
 #include "main.h"
 
+/* function prototypes */
+static void disp_hex(const char *const hex, const int len);
+
 int main(void)
 {
 	printf("[ fixed xor ]\n");
 
-	/* code */
+	char hex1[strlen(str1) / 2], hex2[strlen(str2) / 2];
+	int len1=0, len2=0;
+
+	/* check if input strings are of equal lengths */
+	len1 = strlen(str1);
+	len2 = strlen(str2);
+	if (len1 != len2) {
+		fprintf(stderr, "unequal input string lengths!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* display strings */
+	printf("string 1: %s\nstring 2: %s\n", str1, str2);
+
+	/* convert hex encoded strings to array of hex bytes */
+	str_to_hex(str1, hex1);
+	str_to_hex(str2, hex2);
+
+	/* display array of hex bytes */
+	printf("hex 1: ");
+	disp_hex(hex1, len1 / 2);
+	printf("hex 2: ");
+	disp_hex(hex2, len2 / 2);
 
 	return EXIT_FAILURE;
+}
+
+static void disp_hex(const char *const hex, const int len)
+{
+	int i;
+	for (i=0; i<len-1; ++i)
+		printf("%02x, ", hex[i]);
+	printf("%02x\n", hex[i]);
 }
--- set1/fixed_xor/main.h
+++ set1/fixed_xor/main.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "hex_to_base64.h"
+
+const char str1[] = "1c0111001f010100061a024b53535009181c";
+const char str2[] = "686974207468652062756c6c277320657965";
 
 #endif
