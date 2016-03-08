a78da0cd20fc8165219e9207afdbaf0ad743825c
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-08 14:34:43 +0530

    added letter frequency table

--- set1/q3/main.h
+++ set1/q3/main.h
@@ -8,6 +8,17 @@
 #include "hex_to_base64.h"
 #include "xor.h"
 
+/* letter frequency table */
+const double letter_freq[] = {
+	8.167,	1.492,	2.782,	4.253,	12.702,	/* a - e */
+	2.228,	2.015,	6.094,	6.966,	0.153,	/* f - j */
+	0.772,	4.025,	2.406,	6.749,	7.507,	/* k - o */
+	1.929,	0.095,	5.987,	6.327,	9.056,	/* p - t */
+	2.758,	0.978,	2.361,	0.150,	1.974,	/* u - y */
+	0.074,					/* z     */
+};
+
+/* encrypted hex encoded string */
 const char enc[] = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 
 #endif
