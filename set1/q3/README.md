619be4ca233b2905b78c01ff707cceb2f59eec8a
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-08 12:54:16 +0530

    added q3 files

---
+++ set1/q3/README.md
@@ -0,0 +1,6 @@
+Single-byte XOR Cipher
+======================
+
+Decrypts the message encrypted by a single-byte XOR cipher.  
+Solution to the Matasano Crypto Challenges (Set 1, Q3. Single-byte XOR cipher).  
+Link: http://cryptopals.com/sets/1/challenges/3/
---
+++ set1/q3/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hex_to_base64.h"
+#include "xor.h"
+
+const char enc[] = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
+#endif
