498b52b35e7641ba0b584a9a5c727d18f103cffc
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-08 20:39:31 +0530

    removed length parameter

--- include/decrypt_xor.c
+++ include/decrypt_xor.c
@@ -1,20 +1,17 @@
 #include "decrypt_xor.h"
 
 /* decrypt single-byte XOR cipher */
-char decrypt_sbyte_xor(
-	const int len,
-	const char *const b1,
-	char *const b2)
+char decrypt_sbyte_xor(const char *const b1, char *const b2)
 {
-	char key=0, tmp[len];
+	char key=0, tmp[strlen(b1)];
 	int c;
 	double err, min=DBL_MAX, lhist[26];
 	for (c=0; c<128; ++c) {
-		sbyte_xor(b1, c, tmp);
-		if (!calc_letter_hist(len, tmp, lhist))
+		sbyte_xor(b1, c, tmp);		/* XOR bytes with key */
+		if (!calc_lhist(tmp, lhist))	/* skip if non-printable */
 			continue;
-		err = cmp_letter_hist(lhist);
-		if (err <= min) {
+		err = cmp_lhist(lhist);		/* calculate comparison error */
+		if (err <= min) {		/* store key & smallest error */
 			key = c;
 			min = err;
 		}
@@ -24,35 +21,33 @@ char decrypt_sbyte_xor(
 }
 
 /* calculate normalized letter frequency histogram of bytes */
-bool calc_letter_hist(
-	const int len,
-	const char *const b,
-	double *const h)
+bool calc_lhist(const char *b, double *const h)
 {
-	int i;
-	for (i=0; i<26; ++i)
+	int i, len = strlen(b);
+	for (i=0; i<26; ++i)			/* clear histogram */
 		h[i] = 0;
-	for (i=0; i<len; ++i) {
-		if (!isprint(b[i]))
+	while (*b) {
+		if (!isprint(*b))		/* check if non-printable */
 			return false;
-		if (isalpha(b[i]))
-			h[tolower(b[i]) - 'a']++;
+		if (isalpha(*b))		/* check if alphabet */
+			++h[tolower(*b) - 'a'];
+		++b;
 	}
-	for (i=0; i<26; ++i)
+	for (i=0; i<26; ++i)			/* normalize histogram */
 		h[i] /= len;
 	return true;
 }
 
-/* compare letter frequency histograms */
-/* Chi-square histogram comparison     */
+/* compare letter frequency histograms         */
+/* Chi-square histogram comparison             */
 /* d(h1,h2) = sum( (h1[i] - h2[i])^2 / h1[i] ) */
-double cmp_letter_hist(const double *const h)
+double cmp_lhist(const double *const h)
 {
 	int i;
 	double tmp, err=0;
 	for (i=0; i<26; ++i) {
-		tmp = letter_lut[i] - h[i];
-		err += tmp * tmp / letter_lut[i];
+		tmp = lhist_lut[i] - h[i];
+		err += tmp * tmp / lhist_lut[i];
 	}
 	return err;
 }
--- include/decrypt_xor.h
+++ include/decrypt_xor.h
@@ -4,11 +4,12 @@
 #include <ctype.h>
 #include <float.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "xor.h"
 
 /* normalized letter frequency histogram look-up table */
-static const double letter_lut[26] = {
+static const double lhist_lut[26] = {
 	.08167,	.01492,	.02782,	.04253,	.12702,	/* a - e */
 	.02228,	.02015,	.06094,	.06966,	.00153,	/* f - j */
 	.00772,	.04025,	.02406,	.06749,	.07507,	/* k - o */
@@ -17,14 +18,8 @@ static const double letter_lut[26] = {
 	.00074,					/* z     */
 };
 
-char decrypt_sbyte_xor(
-	const int len,
-	const char *const h1,
-	char *const h2);
-bool calc_letter_hist(
-	const int len,
-	const char *const b,
-	double *const h);
-double cmp_letter_hist(const double *const h);
+char decrypt_sbyte_xor(const char *const b1, char *const b2);
+bool calc_lhist(const char *b, double *const h);
+double cmp_lhist(const double *const h);
 
 #endif
