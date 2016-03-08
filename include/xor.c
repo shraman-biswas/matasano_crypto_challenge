4f74fc814f4d98ec021fb1a752efc2b4d464e69a
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-08 13:51:11 +0530

    fixed null checking

--- include/xor.c
+++ include/xor.c
@@ -3,13 +3,15 @@
 /* XOR two equal-length array of hex bytes */
 void fixed_xor(const char *h1, const char *h2, char *h3)
 {
-	while (*h3++ = *h1++ ^ *h2++);
+	while (*h1)
+		*h3++ = *h1++ ^ *h2++;
 	*h3 = 0;
 }
 
 /* XOR array of hex bytes with single byte */
 void sbyte_xor(const char *h1, const char c, char *h2)
 {
-	while (*h2++ = *h1++ ^ c);
+	while (*h1)
+		*h2++ = *h1++ ^ c;
 	*h2 = 0;
 }
