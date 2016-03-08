3834314aa1a81f28ebfb93629044d3ffcfeaa321
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-08 20:03:30 +0530

    fixed compiler warnings

--- include/base64.c
+++ include/base64.c
@@ -5,8 +5,10 @@ void base64(const char *b1, char *b2)
 {
 	while (*b1) {
 		*b2++ = base64_lut[ *b1 >> 2 ];
-		*b2++ = base64_lut[ ((*b1++ << 4) & 0x3f) | (*b1 >> 4) ];
-		*b2++ = base64_lut[ ((*b1++ << 2) & 0x3f) | (*b1 >> 6) ];
+		*b2++ = base64_lut[ ((*b1 << 4) & 0x3f) | (*(b1+1) >> 4) ];
+		b1++;
+		*b2++ = base64_lut[ ((*b1 << 2) & 0x3f) | (*(b1+1) >> 6) ];
+		b1++;
 		*b2++ = base64_lut[ *b1++ & 0x3f ];
 	}
 	*b2 = 0;
