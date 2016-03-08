66b58350d43d037d19e21bda361fccee64fa42da
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-08 19:52:43 +0530

    updated files to use new include files

--- set1/q2/main.c
+++ set1/q2/main.c
@@ -5,7 +5,7 @@ int main(void)
 	printf("[ fixed XOR ]\n\n");
 
 	const int len = strlen(str1) / 2;
-	char hex1[len], hex2[len], res[len], str[2 * len];
+	char bytes1[len], bytes2[len], res[len], str[2 * len];
 
 	/* check if input strings are of equal lengths */
 	if ((strlen(str2) / 2) != len) {
@@ -14,19 +14,20 @@ int main(void)
 	}
 
 	/* display strings */
-	printf("hex string 1:\n%s\nhex string 2:\n%s\n\n", str1, str2);
+	printf("hex encoded string 1:\n%s\n", str1);
+	printf("hex encoded string 2:\n%s\n\n", str2);
 
-	/* convert hex encoded strings to array of hex bytes */
-	str_to_hex(str1, hex1);
-	str_to_hex(str2, hex2);
+	/* convert hex encoded strings to bytes */
+	str_to_bytes(str1, bytes1);
+	str_to_bytes(str2, bytes2);
 
-	/* applying fixed xor */
-	fixed_xor(hex1, hex2, res);
+	/* apply fixed xor */
+	fixed_xor(bytes1, bytes2, res);
+	printf("fixed xor result:\n%s\n\n", res);
 
-	/* display result */
-	printf("result:\n%s\n\n", res);
-	hex_to_str(res, str);
-	printf("hex string:\n%s\n\n", str);
+	/* bytes to string conversion */
+	bytes_to_str(res, str);
+	printf("hex encoded string:\n%s\n\n", str);
 
-	return EXIT_FAILURE;
+	return EXIT_SUCCESS;
 }
