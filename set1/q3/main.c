a7928b6070fc90ec6f596460d934672c05129240
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-08 14:15:52 +0530

    implemented single byte XOR

--- set1/q3/main.c
+++ set1/q3/main.c
@@ -2,7 +2,24 @@
 
 int main(void)
 {
-	printf("[ single-byte XOR cipher ]\n");
+	printf("[ single-byte XOR cipher ]\n\n");
+
+	const int len = strlen(enc) / 2;
+	char hex[len], dec[len];
+
+	/* display encrypted string */
+	printf("encrypted:\n%s\n\n", enc);
+
+	/* hex encoded string to array of hex bytes conversion */
+	str_to_hex(enc, hex);
+	printf("hex string:\n%s\n\n", hex);
+
+	/* decryption */
+	unsigned char c;
+	for (c=0; c<128; ++c) {
+		sbyte_xor(hex, c, dec);
+		printf("%s\n", dec);
+	}
 
 	return EXIT_FAILURE;
 }
